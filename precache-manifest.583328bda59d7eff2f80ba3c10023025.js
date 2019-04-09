b3a9fcab02f1760ab6306ce0cf16f0d863032108
Messiah <Taylor.Jonathan@aol.com>
2019-04-09 17:25:02 -0400

    Updates

---
+++ precache-manifest.583328bda59d7eff2f80ba3c10023025.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dbd2b72124fd8e202ae595104899f85",
+    "url": "/portfolio/static/media/2-large.1dbd2b72.jpg"
+  },
+  {
+    "revision": "ee006ef04ef970aadf9d",
+    "url": "/portfolio/static/css/main.a8c437b4.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.784d01ab.js"
+  },
+  {
+    "revision": "c699124813980c233844",
+    "url": "/portfolio/static/js/2.52581f94.chunk.js"
+  },
+  {
+    "revision": "58c2f7439492511e1d3d25128439092b",
+    "url": "/portfolio/static/media/spacex1.58c2f743.png"
+  },
+  {
+    "revision": "ee006ef04ef970aadf9d",
+    "url": "/portfolio/static/js/main.77d298ef.chunk.js"
+  },
+  {
+    "revision": "795884adaab58521b2242553a31a7956",
+    "url": "/portfolio/static/media/spacex2.795884ad.png"
+  },
+  {
+    "revision": "8294e87468f14fe9b59b52460c8197d1",
+    "url": "/portfolio/static/media/spacex3.8294e874.png"
+  },
+  {
+    "revision": "f6c61760053c965021fe333c3ac9497b",
+    "url": "/portfolio/static/media/spacex4.f6c61760.png"
+  },
+  {
+    "revision": "fee39bc24ad6dde832c3dccb7a34358b",
+    "url": "/portfolio/static/media/charlotte.fee39bc2.jpg"
+  },
+  {
+    "revision": "c2468d4aca00b43aba0beff0f36c979e",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8bd9cdfc419d53eb931e606e73d400d8.js"
+  "/portfolio/precache-manifest.583328bda59d7eff2f80ba3c10023025.js"
 );
 
 workbox.clientsClaim();
