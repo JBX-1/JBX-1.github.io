de9fcf52e44a6145922b1a80cbcef23a25077b4b
Messiah <Taylor.Jonathan@aol.com>
2019-04-11 06:48:50 -0400

    Updates

---
+++ precache-manifest.f10e3720dcfef212413e0f89b1fc5e3e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f7c2bf99b0daf451f7515c1b929f3de",
+    "url": "/portfolio/static/media/todolist2.4f7c2bf9.png"
+  },
+  {
+    "revision": "f314cdfc91584702e949",
+    "url": "/portfolio/static/css/main.0f5e9f73.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.784d01ab.js"
+  },
+  {
+    "revision": "295399e67f74ed5baa10",
+    "url": "/portfolio/static/js/2.3f0fbed1.chunk.js"
+  },
+  {
+    "revision": "1dbd2b72124fd8e202ae595104899f85",
+    "url": "/portfolio/static/media/2-large.1dbd2b72.jpg"
+  },
+  {
+    "revision": "58c2f7439492511e1d3d25128439092b",
+    "url": "/portfolio/static/media/spacex1.58c2f743.png"
+  },
+  {
+    "revision": "2fd706ac5dfc8c5c5b4e88f75e4e95cb",
+    "url": "/portfolio/static/media/todolist1.2fd706ac.png"
+  },
+  {
+    "revision": "dbdb630b9f462ef38fbf2d0d4b40f8b1",
+    "url": "/portfolio/static/media/thecrimemachine.dbdb630b.png"
+  },
+  {
+    "revision": "f314cdfc91584702e949",
+    "url": "/portfolio/static/js/main.cdde6eb3.chunk.js"
+  },
+  {
+    "revision": "6503e08e11ce0f771f6c6409bc715b82",
+    "url": "/portfolio/static/media/todolist3.6503e08e.png"
+  },
+  {
+    "revision": "b40199cb7abe9022b776aa26a6140e69",
+    "url": "/portfolio/static/media/todolist4.b40199cb.png"
+  },
+  {
+    "revision": "795884adaab58521b2242553a31a7956",
+    "url": "/portfolio/static/media/spacex2.795884ad.png"
+  },
+  {
+    "revision": "8294e87468f14fe9b59b52460c8197d1",
+    "url": "/portfolio/static/media/spacex3.8294e874.png"
+  },
+  {
+    "revision": "f6c61760053c965021fe333c3ac9497b",
+    "url": "/portfolio/static/media/spacex4.f6c61760.png"
+  },
+  {
+    "revision": "fee39bc24ad6dde832c3dccb7a34358b",
+    "url": "/portfolio/static/media/charlotte.fee39bc2.jpg"
+  },
+  {
+    "revision": "0313be65976e415285465cf555c87870",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.eafaf7d79083595de099c991e7320a5d.js"
+  "/portfolio/precache-manifest.f10e3720dcfef212413e0f89b1fc5e3e.js"
 );
 
 workbox.clientsClaim();
